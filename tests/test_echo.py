6b811ad19c555e8776f29fb752fde87876e37531
BalconyJH <balconyjh@gmail.com>
2023-11-14 06:48:06 +0800

    :white_check_mark: 重写单测

--- tests/test_echo.py
+++ tests/test_echo.py
@@ -1,32 +1,18 @@
-
 import pytest
-from nonebot.adapters.onebot.v11 import MessageEvent, Message
-from nonebot.adapters.onebot.v11.event import Sender
+from nonebot.adapters.onebot.v11 import Bot, Message
 from nonebug import App
+
+from tests.utils import fake_group_message_event_v11
 
 
 @pytest.mark.asyncio
 async def test_echo(app: App):
     from nonebot_plugin_bh3_elysian_realm import elysian_realm
 
-    event = MessageEvent(
-        time=1699759597,
-        self_id=12345679,
-        message=Message("/乐土 人律"),
-        user_id=1234567,
-        message_id=11111,
-        raw_message="/乐土 人律",
-        sub_type="test",
-        message_type="test",
-        sender=Sender(
-            user_id=1234567,
-        ),
-        font=0,
-        post_type="message",
-    )
-
     async with app.test_matcher(elysian_realm) as ctx:
-        bot = ctx.create_bot()
+        bot = ctx.create_bot(base=Bot)  # noqa: F811
+        event = fake_group_message_event_v11(message=Message("/乐土人律"))
+
         ctx.receive_event(bot, event)
         ctx.should_call_send(event, "指定的角色是人律", result=None)
-        ctx.should_finished(elysian_realm)
+        ctx.should_finished()
