ad3281e285e668c7a43573ebb9894f1056279906
BalconyJH <balconyjh@gmail.com>
2023-11-15 21:55:20 +0800

    :bug: 修复clone函数额外判断

--- nonebot_plugin_bh3_elysian_realm/__init__.py
+++ nonebot_plugin_bh3_elysian_realm/__init__.py
@@ -1,5 +1,6 @@
+
 from nonebot import on_command
-from nonebot.adapters.onebot.v11 import Message
+from nonebot.adapters.onebot.v11 import Message as OBV11Message
 from nonebot.internal.matcher import Matcher
 from nonebot.internal.params import ArgPlainText
 from nonebot.params import CommandArg
@@ -7,7 +8,8 @@ from nonebot.permission import SUPERUSER
 from nonebot.plugin import PluginMetadata
 from nonebot_plugin_bh3_elysian_realm.nickname_parser import find_key_by_value
 from .config import plugin_config
-from nonebot_plugin_saa import MessageFactory, Text, Image
+import nonebot_plugin_saa as saa
+from .resources import find_image
 
 __plugin_meta__ = PluginMetadata(
     name="乐土攻略",
@@ -26,14 +28,13 @@ __plugin_meta__ = PluginMetadata(
     },
 )
 
-from .resources import find_image
 
 elysian_realm = on_command("乐土攻略", aliases={"乐土", "乐土攻略"}, priority=7)
 update_elysian_realm = on_command("乐土更新", aliases={"乐土更新"}, priority=7, permission=SUPERUSER)
 
 
 @elysian_realm.handle()
-async def handle_function(matcher: Matcher, args: Message = CommandArg()):
+async def handle_function(matcher: Matcher, args: OBV11Message = CommandArg()):
     if args.extract_plain_text():
         matcher.set_arg("role", args)
 
@@ -42,8 +43,11 @@ async def handle_function(matcher: Matcher, args: Message = CommandArg()):
 async def got_introduction(role: str = ArgPlainText()):
     nickname = await find_key_by_value(plugin_config.nickname_path, role)
     if nickname is None:
-        msg_builder = MessageFactory(Text("未找到指定角色"))
+        msg_builder = saa.Text("未找到指定角色")
         await msg_builder.finish()
     else:
-        msg_builder = MessageFactory(Image(await find_image(nickname)))
+        msg_builder = saa.Image(await find_image(nickname))
         await msg_builder.finish()
+
+
+# @update_elysian_realm.handle()
--- nonebot_plugin_bh3_elysian_realm/resources.py
+++ nonebot_plugin_bh3_elysian_realm/resources.py
@@ -1,6 +1,7 @@
 import os
 import re
 import subprocess
+from pathlib import Path
 
 from nonebot import logger
 from tqdm import tqdm
@@ -20,7 +21,7 @@ async def git_pull():
 
     # 确保目录存在
     if not os.path.exists(repo_dir):
-        print(f"目录 {repo_dir} 不存在")
+        logger.error(f"目录 {repo_dir} 不存在")
         return
 
     # 更改当前工作目录
@@ -31,7 +32,7 @@ async def git_pull():
         with subprocess.Popen(clone_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True) as process:
             # 使用 tqdm 显示进度
             if "Already up to date." in process.stdout.read():
-                print("ElysianRealm-Data已是最新版本")
+                logger.info("ElysianRealm-Data已是最新版本")
             else:
                 with tqdm(desc="仓库更新中") as pbar:
                     # print(process.stdout.read())
@@ -50,7 +51,7 @@ repository = "https://github.com/MskTmi/ElysianRealm-Data"
 
 
 async def git_clone(repository_url: str = repository):
-    clone_command = ["git", "clone", "--progress", "--depth=4", repository_url, "resources/images"]
+    clone_command = ["git", "clone", "--progress", "--depth=1", repository_url, "resources/images"]
 
     try:
         # 使用 Popen 而不是 run
@@ -58,9 +59,6 @@ async def git_clone(repository_url: str = repository):
             # 使用 tqdm 显示进度
             with tqdm(desc="仓库克隆中") as pbar:
                 for line in process.stderr:  # git clone 的进度信息在 stderr 中
-                    if "fatal: destination path" in line:
-                        print("data目录下已存在ElysianRealm-Data, 请勿重复获取")
-                        break
                     speed_match = re.search(r"\|\s*([\d.]+\s*[\w/]+/s)", line)
                     if speed_match:
                         speed = speed_match.group(1)
@@ -69,7 +67,7 @@ async def git_clone(repository_url: str = repository):
 
         # 检查进程是否成功完成
         if process.returncode == 0:
-            print("乐土攻略获取完成")
+            logger.info("乐土攻略获取完成")
 
     except subprocess.CalledProcessError as e:
         error_info = e.stderr
@@ -79,27 +77,30 @@ async def git_clone(repository_url: str = repository):
             logger.error(f"clone出现异常:\n{error_info}")
 
 
-async def get_git_repository_url(repository_url: str) -> bool:
+async def get_git_repository_url(repository_url: str, path: Path) -> bool:
     """
-    异步地检查当前目录是否为指定的 Git 仓库。
+    异步地检查指定目录是否为指定的 Git 仓库。
 
-    此函数通过执行 Git 命令来获取当前目录 Git 仓库的远程 URL。
+    此函数通过在指定目录执行 Git 命令来获取 Git 仓库的远程 URL。
     然后，它会将这个 URL 与提供的 URL 进行比较。
 
     参数:
         repository_url (str): 要检查的 Git 仓库的 URL。
+        path (Path): 要检查的目录路径。
 
     返回:
-        bool: 如果当前目录是指定的 Git 仓库，则返回 True；否则返回 False。
+        bool: 如果指定目录是指定的 Git 仓库，则返回 True；否则返回 False。
 
     异常:
         subprocess.CalledProcessError: 如果在执行 Git 命令时出错，将捕获此异常并返回 False。
 
     注意:
-        这个函数假设 'git' 命令在系统路径上可用，并且当前目录是一个 Git 仓库。
-        如果当前目录不是 Git 仓库，或者 'git' 命令无法执行，函数将返回 False。
+        这个函数假设 'git' 命令在系统路径上可用。
+        如果指定目录不是 Git 仓库，或者 'git' 命令无法执行，函数将返回 False。
     """
+    original_cwd = Path.cwd()  # 保存原始工作目录
     try:
+        os.chdir(path)  # 改变工作目录到指定路径
         remote_url = subprocess.check_output(
             ["git", "config", "--get", "remote.origin.url"],
             stderr=subprocess.STDOUT
@@ -111,10 +112,19 @@ async def get_git_repository_url(repository_url: str) -> bool:
             return False
     except subprocess.CalledProcessError:
         return False
+    finally:
+        os.chdir(original_cwd)  # 恢复原始工作目录
 
 
-async def find_image(role: str) -> str:
+async def find_image(role: str) -> bytes:
     """根据传入的角色名，返回对应的图片"""
     image_path = plugin_config.image_path / f"{role}.jpg"
-    print(image_path)
+    with open(image_path, "rb") as image_file:
+        image = image_file.read()
+    return image
+
+
+async def find_image_path(role: str) -> Path:
+    """根据传入的角色名，返回对应的图片路径"""
+    image_path = plugin_config.image_path / f"{role}.jpg"
     return image_path
