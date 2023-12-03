155bffc22b9033c0c3af5dd96e05f48f50ce0316
BalconyJH <balconyjh@gmail.com>
2023-12-03 20:00:40 +0800

    :white_check_mark: 更新update_elysian_realm測試

--- tests/test_matcher.py
+++ tests/test_matcher.py
@@ -1,7 +1,6 @@
-from pathlib import Path
-
 import pytest
 from nonebug import App
+from nonebot import get_driver
 from pytest_mock import MockerFixture
 from nonebug_saa import should_send_saa
 from nonebot_plugin_saa import MessageFactory
@@ -9,6 +8,21 @@ from nonebot.adapters.onebot.v11 import Bot as OBV11Bot
 from nonebot.adapters.onebot.v11 import Message as OBV11Message
 
 from tests.utils import fake_group_message_event_v11
+
+
+@pytest.mark.asyncio
+async def test_update_elysian_realm(app: App, mocker: MockerFixture):
+    from nonebot_plugin_bh3_elysian_realm.plugins import update_elysian_realm
+
+    mocker.patch.object(get_driver().config, "superusers", {"10"})
+
+    async with app.test_matcher(update_elysian_realm) as ctx:
+        v11bot = ctx.create_bot(base=OBV11Bot)
+        v11event = fake_group_message_event_v11(message=OBV11Message("/乐土更新"), sender={"role": "owner"})
+
+        ctx.receive_event(v11bot, v11event)
+        ctx.should_call_send(v11event, "更新成功")
+        ctx.should_finished()
 
 
 @pytest.mark.asyncio
@@ -23,31 +37,6 @@ async def test_none_nickname(app: App):
         should_send_saa(
             ctx,
             MessageFactory("未找到指定角色"),
-            v11bot,
-            event=v11event,
-        )
-        ctx.should_finished()
-
-
-@pytest.mark.asyncio
-async def test_got_introduction(app: App, mocker: MockerFixture):
-    import nonebot_plugin_saa as saa
-
-    from nonebot_plugin_bh3_elysian_realm.plugins import elysian_realm
-
-    print(Path(__file__).parent / "Human.jpg")
-    image = saa.Image(Path(__file__).parent / "Human.jpg")
-    mocker.patch(image)
-
-    async with app.test_matcher(elysian_realm) as ctx:
-        v11bot = ctx.create_bot(base=OBV11Bot)
-        v11event = fake_group_message_event_v11(message=OBV11Message("/乐土人律"))
-
-        # todo: find a better way to mock
-        ctx.receive_event(v11bot, v11event)
-        should_send_saa(
-            ctx,
-            MessageFactory(image),
             v11bot,
             event=v11event,
         )
