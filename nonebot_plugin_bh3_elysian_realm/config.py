7296e0aae8254cbf3f45c58cd7458735253e8b68
BalconyJH <balconyjh@gmail.com>
2023-12-15 22:46:45 +0800

    :seedling: 添加微博爬虫

--- nonebot_plugin_bh3_elysian_realm/config.py
+++ nonebot_plugin_bh3_elysian_realm/config.py
@@ -8,6 +8,9 @@ class Config(BaseSettings, extra=Extra.ignore):
     nickname_path = Path(__file__).parent / "resources" / "nickname.json"
     image_path = Path(__file__).parent / "resources" / "images"
     image_repository = "https://github.com/MskTmi/ElysianRealm-Data"
+    user_agent = ("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                  "Chrome/86.0.4240.111 Safari/537.36")
+    cookie = ""
 
 
 plugin_config = Config.parse_obj(get_driver().config)
